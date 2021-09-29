77b70c43c1681b1176020f5651393b80b79fef6e
Piyush Jain <piyushmbm45@gmail.com>
2021-09-29 09:31:15 +0530

    file directory improvement

--- utils/logger.js
+++ utils/logger.js
@@ -3,23 +3,16 @@ const { createLogger, format, transports } = require("winston");
 const getLogger = createLogger({
   transports: [
     new transports.File({
-      filename: "../logs/info.log",
+      filename: "info.log",
       level: "info",
-      format: format.combine(
-        format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
-        format.align(),
-        format.printf(
-          (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
-        )
-      ),
+      format: format.combine(format.timestamp(), format.json()),
     }),
     new transports.File({
-      filename: "../logs/error.log",
+      filename: "error.log",
       level: "error",
       format: format.combine(format.timestamp(), format.json()),
     }),
   ],
 });
 
-
-module.exports = {getLogger}
+module.exports = { getLogger };
