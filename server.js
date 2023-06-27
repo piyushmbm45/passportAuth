7c1916ec730ec48b8a7590874af76f90def85b72
Piyush Jain <piyushmbm45@gmail.com>
2023-06-27 08:13:32 +0530

    Improve - added favicon

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-
-
 // Routes
-app.use('/',require('./config/sessions'))
-app.use('/', require('./routes/homeRoute'))
-app.use('/', require('./routes/googleRoute'))
-app.use('/', require('./routes/facebookRoute'))
-app.use('/',require('./routes/twitterRoute'))
+app.use('/', require('./config/sessions'));
+app.use('/', require('./routes/homeRoute'));
+app.use('/', require('./routes/googleRoute'));
+app.use('/', require('./routes/facebookRoute'));
+app.use('/', require('./routes/twitterRoute'));
 
-app.listen(PORT, () => console.log(`Listening on Port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
