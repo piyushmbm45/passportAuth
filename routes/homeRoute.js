2fd63e3a22fb2de945e6f2fbb826b5bbcc13bc15
Piyush Jain <piyushmbm45@gmail.com>
2023-06-28 07:54:45 +0530

    middleware change

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,25 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express();
-const passport = require("passport");
-const bcrypt = require("bcrypt");
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const User = require("../utils/userSchema");
-const flash = require("express-flash");
+const User = require('../utils/userSchema');
+const flash = require('express-flash');
 // const methodOverride = require("method-override");
 
-const initializePassport = require("../middleware/passportLocalConfig");
+const initializePassport = require('../middleware/passportLocalConfig');
 initializePassport(passport);
 
 router.use(flash());
-router.use(express.json());
-router.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+
 // middleware to use ejs template engine
-router.set("view engine", "ejs");
-router.use(express.static("public"));
+// router.set("view engine", "ejs");
+// router.use(express.static("public"));
 
 router.use(passport.initialize());
 router.use(passport.session());
@@ -27,7 +22,7 @@ router.use(passport.session());
 // ------ form method for logout - delete method at the place of POST method
 // router.use(methodOverride("_method"));
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const hash = await bcrypt.hash(req.body.password, saltRounds);
   const newUser = await new User({
     name: req.body.name,
@@ -36,42 +31,42 @@ router.post("/", async (req, res) => {
   });
   newUser.save((err) => {
     if (!err) {
-      req.flash("info", "You are registered Now You can login");
-      res.redirect("/login");
+      req.flash('info', 'You are registered Now You can login');
+      res.redirect('/login');
     } else {
       if (err.code === 11000) {
-        req.flash("info", "User Already Exist");
-        res.redirect("/");
+        req.flash('info', 'User Already Exist');
+        res.redirect('/');
       } else {
-        res.redirect("/");
+        res.redirect('/');
       }
     }
   });
 });
 
-router.get("/", checkNotAuthenticated, (req, res) => {
-  res.render("home");
+router.get('/', checkNotAuthenticated, (req, res) => {
+  res.render('home');
 });
 
 // for login route
-router.get("/login", checkNotAuthenticated, (req, res) => {
-  res.render("login");
+router.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login');
 });
 router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: "/login",
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login',
     failureFlash: true,
     session: true,
   }),
   (req, res) => {
     console.log(req.session);
-    res.redirect("/secret");
+    res.redirect('/secret');
   }
 );
 
 // --------logout route
-router.post("/logout", (req, res, next) => {
+router.post('/logout', (req, res, next) => {
   const email = req.user.username;
   console.log(email);
   User.findOneAndUpdate(
@@ -87,12 +82,12 @@ router.post("/logout", (req, res, next) => {
   );
   req.logOut();
   console.log(req.session);
-  res.redirect("/");
+  res.redirect('/');
 });
 
 // secret route
-router.get("/secret", checkAuthenticated, (req, res) => {
-  res.render("secret", { user: req.user });
+router.get('/secret', checkAuthenticated, (req, res) => {
+  res.render('secret', { user: req.user });
 });
 
 // need authenticated user to see our secret route
@@ -100,13 +95,13 @@ function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect("/login");
+  res.redirect('/login');
 }
 
 // if user already logged in then we dont want to show him register and login route
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return res.redirect("/secret");
+    return res.redirect('/secret');
   }
   next();
 }
--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// middleware to use ejs template engine
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 // Routes
 app.use('/', require('./config/sessions'));
 app.use('/', require('./routes/homeRoute'));
